08e79027e915d6940c4a8a1d5e0bafa1e0bd9f73
akash <akashak1602@gmail.com>
2026-02-12 16:11:04 +0530

    added the jishnu

--- src/Pages/Invitationcard/ClinetDetails/index.js
+++ src/Pages/Invitationcard/ClinetDetails/index.js
@@ -1,9 +1,11 @@
 import sreelaksmi from "./sreelakshmi-anirudh";
 import sangeeth from "./sangeeth-amrutha";
 import arjun from "./arjun-meera";
+import jishnuVandana from "./jishnuVandana";
 
 export const allWeddings = {
   "sreelakshmi-anirudh": sreelaksmi,
   "sangeeth-amrutha": sangeeth,
   "arjun-meera": arjun,
+  "jishnu-vandana": jishnuVandana,
 };
---
+++ src/Pages/Invitationcard/ClinetDetails/jishnuVandana.js
@@ -0,0 +1,46 @@
+const jishnuVandana = {
+  slug: "jishnu-vandana",
+
+  couple: {
+    bride: "Dr. Vandana S.S",
+    groom: "Jishnu K.P",
+
+    parents: {
+      groomParents: "Mr. Sajeevan K.P & Mrs. Dajirani K.P",
+      brideParents: "Dr. Sudeep & Sheeba K.T",
+    },
+
+    familyDetails: {
+      groomFamily: "Kollompadikkal (H), Vattoli",
+      brideFamily: "Edakkatt, Arur",
+      sharedBy: "Vyshna K.P & Kollompadikkal family",
+    },
+  },
+
+  wedding: {
+    day: "Sunday",
+    date: "08",
+    month: "March",
+    year: "2026",
+    time: "11:30 AM - 12:15 PM",
+    venue: "Kalleri Auditorium",
+    note: "Marriage will be solemnized on the same day",
+       mapLocation:
+      "https://maps.app.goo.gl/eQJ5BJVSNwduJYNd6?g_st=aw",
+  },
+
+  reception: {
+    day: "Sunday",
+    date: "08",
+    month: "March",
+    year: "2026",
+    time: "4:00 PM - 9:00 PM",
+    venue: "Mehfil Auditorium, Kuttiadi",
+  },
+
+  contact: {
+    phone: "9895060950",
+  },
+};
+
+export default jishnuVandana;
